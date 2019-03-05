94db0fb0e4da9e9b50d95f3b57d7c72286aff861
CARLES PEREZ ONIELFA <cperezon18.alumnes@ub052229.mat.ub.edu>
2019-03-05 09:49:50 +0100

    Commit inicial del projecte

---
+++ Project/app/src/main/java/edu/ub/pis/pis_16/pis_16/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.ub.pis.pis_16.pis_16
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
