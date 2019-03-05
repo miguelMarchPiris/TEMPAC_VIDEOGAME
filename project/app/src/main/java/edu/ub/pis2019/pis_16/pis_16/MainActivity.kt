1423fa6869fe83a3f67464f23855e95bfc33c8d2
CARLES PEREZ ONIELFA <cperezon18.alumnes@ub052229.mat.ub.edu>
2019-03-05 10:06:45 +0100

    Projecte inicial correcte afegit

--- project/app/src/main/java/edu/ub/pis2019/pis_16/pis_16/MainActivity.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/pis_16/MainActivity.kt
@@ -1,4 +1,4 @@
-package edu.ub.pis.pis_16.pis_16
+package edu.ub.pis2019.pis_16.pis_16
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
--- project/app/src/test/java/edu/ub/pis2019/pis_16/pis_16/ExampleUnitTest.kt
+++ project/app/src/test/java/edu/ub/pis2019/pis_16/pis_16/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package edu.ub.pis.pis_16.pis_16
+package edu.ub.pis2019.pis_16.pis_16
 
 import org.junit.Test
 
