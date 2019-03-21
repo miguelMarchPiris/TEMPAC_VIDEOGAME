c52e2dbac43789d9351bfff39c79943878873f20
Miguel <mmarchpi@alumnes.ub.edu>
2019-03-21 19:31:05 +0100

    He colocado los elementos básicos del layout de highscore

---
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/HighScore.kt
@@ -0,0 +1,12 @@
+package edu.ub.pis2019.pis_16.tempac
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HighScore : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_highscore)
+    }
+}
