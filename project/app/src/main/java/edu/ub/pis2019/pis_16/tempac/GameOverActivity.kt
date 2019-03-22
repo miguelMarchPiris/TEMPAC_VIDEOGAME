c73655e9048b6b9dc715d7013c1917a484824949
Piotr Piwowarski <piotr95piwowarski@gmail.com>
2019-03-22 02:50:13 +0100

    GameOverActivity splited into fragments

--- project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/GameOverActivity.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/GameOverActivity.kt
@@ -10,34 +10,8 @@ import android.widget.Toast
 
 class GameOverActivity : AppCompatActivity() {
 
-    lateinit var menuButton: Button
-    lateinit var shareButton: Button
-    lateinit var backgroundLayout: ConstraintLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
-        var animBounce = AnimationUtils.loadAnimation(this, R.anim.bounce)
-        var text : TextView = findViewById(R.id.playAgainText)
-        text.startAnimation(animBounce)
-
-        menuButton = findViewById(R.id.menuButton)
-        shareButton = findViewById(R.id.shareButton)
-        backgroundLayout = findViewById(R.id.backgroundLayout)
-
-        menuButton.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Menu Button Action",Toast.LENGTH_SHORT).show()
-        }
-
-        shareButton.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Share Button Action",Toast.LENGTH_SHORT).show()
-        }
-
-        backgroundLayout.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Background Layout Action",Toast.LENGTH_SHORT).show()
-        }
     }
 }
