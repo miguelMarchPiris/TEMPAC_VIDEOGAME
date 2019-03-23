61de5fc4133c6f17169e7440a9e9eea3044c2856
Miguel <mmarchpi@alumnes.ub.edu>
2019-03-23 16:34:54 +0100

    Probando de hacer un commit

--- project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/HighScore.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/HighScore.kt
@@ -2,11 +2,13 @@ package edu.ub.pis2019.pis_16.tempac
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class HighScore : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_highscore)
+        var texto : TextView?
     }
 }
