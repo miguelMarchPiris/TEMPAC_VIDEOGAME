6712ac97fd069816dc20ccf1ba86ad6ca870f5bd
CarlesPOnielfa <98perezcarles@gmail.com>
2019-03-24 09:33:55 +0100

    Fixed undetected merge errors

--- project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/GameOverActivity.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/GameOverActivity.kt
@@ -18,25 +18,7 @@ class GameOverActivity : AppCompatActivity() {
         var text : TextView = findViewById(R.id.playAgainText)
         text.startAnimation(animBounce)
 
-        menuButton = findViewById(R.id.menuButton)
-        shareButton = findViewById(R.id.shareButton)
-        backgroundLayout = findViewById(R.id.backgroundLayout)
 
-        menuButton.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Menu Button Action",Toast.LENGTH_SHORT).show()
-            changeActivityMenu()
-        }
-
-        shareButton.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Share Button Action",Toast.LENGTH_SHORT).show()
-        }
-
-        backgroundLayout.setOnClickListener{
-            //Menu Button Action
-            Toast.makeText(baseContext,"Background Layout Action",Toast.LENGTH_SHORT).show()
-        }
     }
 
     //connects with another activity passed by parameter
