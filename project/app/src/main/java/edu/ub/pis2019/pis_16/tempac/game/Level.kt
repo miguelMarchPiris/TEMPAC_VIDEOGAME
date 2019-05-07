a8e8a8458870530f089bd8bf86ca86b3b9f86e52
Miguel <mmarchpi@alumnes.ub.edu>
2019-05-07 10:00:21 +0200

    level is now working. Now i need to make the generation

--- project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/game/Level.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/game/Level.kt
@@ -5,8 +5,8 @@ import android.graphics.Canvas
 //clase colisionable (los objetos con los que chocas i no pasa nada) i class no colisionable (los objetos no colisionables que no pasa nada cuando xocan.)
 class Level : Drawable {
     var orbs : MutableList<Orb> = mutableListOf<Orb>()
-    var blocks : MutableList<Block> = mutableListOf<Block>()
-
+    var blocks : MutableList <Block> = mutableListOf<Block>()
+    
     init{
         //Instanciamos bloques para hacer pruebas
         //Los bloques tienen un ancho de 80 (se puede modificar en classe block)
