e304452fd8fc168c3a8257b2441ba23acf8671fd
PauSolerValades <pau.soler.valades@gmail.com>
2019-05-08 12:53:14 +0200

    Added pacman images in all resolutions

--- project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/View/GameActivity.kt
+++ project/app/src/main/java/edu/ub/pis2019/pis_16/tempac/View/GameActivity.kt
@@ -3,7 +3,9 @@ package edu.ub.pis2019.pis_16.tempac.View
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import edu.ub.pis2019.pis_16.tempac.R
+import kotlinx.android.synthetic.main.activity_game.*
 
 
 //
@@ -12,6 +14,8 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        btnPause.visibility = View.GONE
 
         //This Part crashes the activity
         /*var animBounce = AnimationUtils.loadAnimation(this, R.anim.bounce)
